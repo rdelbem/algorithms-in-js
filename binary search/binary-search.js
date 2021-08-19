05a40097402b7d18c35f6775aa551391aa150f7e
Rodrigo Del Bem <rodrigodelbem@gmail.com>
2021-08-19 20:53:41 -0300

    binary search

--- binary search/binary-search.js
+++ binary search/binary-search.js
@@ -5,4 +5,33 @@
  * it drastically reduces time.
  *
  * NOTICE, the iteratable data structure, i.e. an array, must be organized, sorted.
+ *
+ *
  */
+
+const arrSorted = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42,
+];
+
+function binarySearch(arr, value) {
+  let start = 0;
+  let end = arr.length - 1;
+  let pivotPoint = Math.floor((start + end) / 2);
+
+  while (arr[pivotPoint] !== value) {
+    console.log(start, pivotPoint, end);
+    if (value < arr[pivotPoint]) {
+      end = pivotPoint - 1;
+    } else {
+      start = pivotPoint + 1;
+    }
+
+    pivotPoint = Math.floor((start + end) / 2);
+  }
+
+  return pivotPoint;
+}
+
+console.log(binarySearch(arrSorted, 40));
